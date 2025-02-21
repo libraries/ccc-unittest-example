2c706bc6932b39d0278633e0058c14cc1b091ae1
mohanson <mohanson@outlook.com>
2025-02-21 20:26:08 +0800

    Vold

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-import { hexFrom, Transaction, SignerCkbPrivateKey, ClientPublicMainnet, WitnessArgs, hashCkb } from "@ckb-ccc/core";
+import { hexFrom, numFrom, Transaction, SignerCkbPrivateKey, ClientPublicMainnet, WitnessArgs, hashCkb } from "@ckb-ccc/core";
+import { numToBytes } from "ckb-ccc";
 import { readFileSync } from "fs";
 import {
   Resource,
-  UnitTestClient,
   Verifier,
 } from "mohanson-unittest";
+
+function hash_sighash_all(tx: Transaction, major: number, other: number[]) {
+  let b = tx.hash();
+  let w = tx.witnesses[major];
+  let l = (w.length - 2) / 2;
+  b = b + hexFrom(numToBytes(l, 8)).slice(2);
+  b = b + hexFrom(w).slice(2);
+  for (const e of other.filter((e) => { e < tx.witnesses.length })) {
+    const w = tx.witnesses[e];
+    const l = (w.length - 2) / 2;
+    b = b + hexFrom(numToBytes(l, 8)).slice(2);
+    b = b + hexFrom(w).slice(2);
+  }
+  for (const e of tx.witnesses.slice(tx.inputs.length)) {
+    const l = (e.length - 2) / 2;
+    b = b + hexFrom(numToBytes(l, 8)).slice(2);
+    b = b + hexFrom(e).slice(2);
+  }
+  return hashCkb(b)
+}
 
 async function main() {
   const prikey = new SignerCkbPrivateKey(new ClientPublicMainnet(), "0x0000000000000000000000000000000000000000000000000000000000000001");
@@ -14,19 +34,25 @@ async function main() {
   const tx = Transaction.default();
 
   const cellMetaJsExec = resource.deployCell(
-    hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/build/ckb-js-vm')), tx, false
+    hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/build/ckb-js-vm')),
   );
-  resource.deployCell(
-    hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/packages/examples/dist/secp256k1_blake160_lock.js')), tx, false
+  const cellMetaJsMain = resource.deployCell(
+    hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/packages/examples/dist/secp256k1_blake160_lock.js')),
   );
-  const dataHash = hashCkb(resource.cells.get(tx.cellDeps.at(-1)!.outPoint)!.outputData);
-  cellMetaJsExec.args = hexFrom("0x0000" + dataHash.slice(2) + "00" + hashCkb(pubkey).slice(2, 42))
-  const inputCell = resource.mockCell(cellMetaJsExec);
-  tx.inputs.push(Resource.createCellInput(inputCell));
+  const cellMetaIArgs = hexFrom("0x0000" + cellMetaJsMain.dataHash.slice(2) + "00" + hashCkb(pubkey).slice(2, 42))
+  const cellMetaI = resource.createCell(
+    numFrom(0),
+    resource.createScriptByData(cellMetaJsExec, cellMetaIArgs),
+    "0x",
+  );
+
+  tx.cellDeps.push(resource.createCellDep(cellMetaJsExec, "code"));
+  tx.cellDeps.push(resource.createCellDep(cellMetaJsMain, "code"));
+  tx.inputs.push(resource.createCellInput(cellMetaI));
   tx.witnesses.push(hexFrom(new WitnessArgs(hexFrom(new Uint8Array(65)), undefined, undefined).toBytes()))
 
-  let mh = await tx.getSignHashInfo(cellMetaJsExec, new UnitTestClient(resource))!;
-  let sg = await prikey._signMessage(mh!.message);
+  const mh = hash_sighash_all(tx, 0, [])
+  const sg = await prikey._signMessage(mh)
   tx.witnesses[0] = hexFrom(new WitnessArgs(sg, undefined, undefined).toBytes())
   const verifier = Verifier.from(resource, tx);
   for (let e of verifier.verify()) {
