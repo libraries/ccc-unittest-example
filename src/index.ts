c61c1be228854ed69af2ef733597340f0e606070
mohanson <mohanson@outlook.com>
2025-02-24 14:54:52 +0800

    Clean code

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,10 @@ async function main() {
   const cellMetaJsExec = resource.deployCell(
     hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/build/ckb-js-vm')), tx, false
   );
-  resource.deployCell(
+  const cellMetaJsMain = resource.deployCell(
     hexFrom(readFileSync('/home/ubuntu/src/ckb-js-vm/packages/examples/dist/secp256k1_blake160_lock.js')), tx, false
   );
-  const dataHash = hashCkb(resource.cells.get(tx.cellDeps.at(-1)!.outPoint)!.outputData);
-  cellMetaJsExec.args = hexFrom("0x0000" + dataHash.slice(2) + "00" + hashCkb(pubkey).slice(2, 42))
+  cellMetaJsExec.args = hexFrom("0x0000" + cellMetaJsMain.codeHash.slice(2) + "00" + hashCkb(pubkey).slice(2, 42))
   const inputCell = resource.mockCell(cellMetaJsExec);
   tx.inputs.push(Resource.createCellInput(inputCell));
   tx.witnesses.push(hexFrom(new WitnessArgs(hexFrom(new Uint8Array(65)), undefined, undefined).toBytes()))
